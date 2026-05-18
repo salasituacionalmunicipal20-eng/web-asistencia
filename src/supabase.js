d25c5ab82ec93ffd84a9bffb014b3868e1949f45
salasituacionalmunicipal20-eng <salasituacionalmunicipal20@gmail.com>
2026-05-18 20:45:42 +0200

    fix: Despliegue definitivo de producción con conexión limpia

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,11 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-// URL limpia, estrictamente SIN el "/rest/v1/" al final
-const supabaseUrl = 'https://tfbzghjjfcaqmkzsxrrs.supabase.co'; 
-const supabaseKey = 'sb_publishable_7dkBdGu87mnWz-acprRjzA_vHBujcwB'; 
+const supabaseUrl = 'https://tfbzghjjfcaqmkzsxrrs.supabase.co'
+const supabaseKey = 'sb_publishable_7dkBdGu87mnWz-acprRjzA_vHBujcwB'
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true
+  }
+})
